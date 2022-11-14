97c756b202a61a5a8cc2b2e16f67562062e5302e
Jorge Guerrero <89558483+GuerreroHJorge@users.noreply.github.com>
2022-11-14 12:22:21 -0600

    Update index.js
    Agregando todas las rutas

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3000;
 
 const rutasUsuarios = require('./src/routes/usuarios');
 app.use('/', rutasUsuarios);
+const rutasRecetas = require('./src/routes/recetas');
+app.use('/', rutasRecetas);
+const rutasRestaurantes = require('./src/routes/restaurantes');
+app.use('/', rutasRestaurantes);
+const rutasBusquedas = require('./src/routes/busquedas');
+app.use('/', rutasBusquedas);
+
 
 app.get('/', (req, res) => {
     res.send('api works');
