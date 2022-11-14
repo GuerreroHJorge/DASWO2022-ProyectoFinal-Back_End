20285c8192b87265dbcfbfb1b039831bbeaf15cf
Jorge Guerrero <89558483+GuerreroHJorge@users.noreply.github.com>
2022-11-14 12:23:37 -0600

    Update busquedas.js
    primer api

---
+++ src/controllers/busquedas.js
@@ -0,0 +1,35 @@
+const controllerBusqueda = {
+    buscarUsuario: (req, res) => {
+        const usuario = req.params.usuario;
+        res.send('Informacion de usuario: ' + usuario);
+    },
+
+    buscarUsuarioPorId: (req, res) => {
+        const id = req.params.id;
+        res.send('Informacion de usuario con id: ' + id);
+    },
+
+    buscarReceta: (req, res) => {
+        const nombre = req.params.nombre;
+        res.send('Informacion receta: ' + nombre);
+    },
+
+    buscarRecetaPorCategoria: (req, res) => {
+        const categoria = req.params.categoria;
+        res.send('Recetas de : ' + categoria);
+    },
+
+    buscarRestaurante: (req, res) => {
+        const nombre = req.params.nombre;
+        res.send('Informacion de restaurante: ' + nombre);
+    },
+
+    buscarRestaurantePorCategoria: (req, res) => {
+        const categoria = req.params.categoria;
+        res.send('Restaurantes de : ' + categoria);
+    }
+
+}
+
+
+module.exports = controllerBusqueda;
